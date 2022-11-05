f8f9faed1e13954460fb536cab0c0e3f8a60cece
Goran Mržljak <goran.mrzljak@gmail.com>
2022-11-05 21:08:56 +0100

    implement github action deploy workflow
    Also some fiddling with tailwind.

---
+++ src/pages/another-page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function AnotherPage(): React.ReactElement {
+  return (<div>{'AnotherPage'}</div>);
+}
---
+++ src/util/util.ts
@@ -0,0 +1,3 @@
+export function classNames(...classes: readonly string[]): string {
+  return classes.filter(Boolean).join(' ')
+}
