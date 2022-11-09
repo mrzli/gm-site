a074675b293fe56f26363de1858dd4b34ced7b43
Goran Mržljak <goran.mrzljak@gmail.com>
2022-11-09 14:10:51 +0100

    update

--- src/pages/habits/tracking/2022.tsx
+++ src/pages/habits/tracking/2022.tsx
@@ -11,51 +11,51 @@ const data: HabitYearData = {
       habits: [
         {
           name: 'Test Habit',
-          values: '//////+',
+          values: '//////++.',
         },
         {
           name: 'Go to Gym (M/W/F)',
-          values: '//////+.',
+          values: '//////+.+',
         },
         {
           name: 'Home Exercise - Func (T/T/S)',
-          values: '///////+',
+          values: '///////+.',
         },
         {
           name: 'Home Exercise - Func - Before 09:00',
-          values: '///////+',
+          values: '///////+.',
         },
         {
           name: 'Running 3km (T/T/S)',
-          values: '////+..+',
+          values: '////+..+.',
         },
         {
           name: 'Running 5km (T/T/S)',
-          values: '////+..+',
+          values: '////+..+.',
         },
         {
           name: 'Running - Before 10:00',
-          values: '///////+',
+          values: '///////+.',
         },
         {
           name: 'Stretch (T/T/S)',
-          values: '///////+',
+          values: '///////+.',
         },
         {
           name: 'Stretch - Before 10:00',
-          values: '///////-',
+          values: '///////-,',
         },
         {
           name: 'Neck Exercise (T/T/S)',
-          values: '///////+',
+          values: '///////+.',
         },
         {
           name: 'Neck Exercise - Before 11:00',
-          values: '///////-',
+          values: '///////-,',
         },
         {
           name: 'Go to Latina Dance Class',
-          values: '//////+.',
+          values: '//////+..',
         },
         {
           name: 'Get to Latina Dance Class on Time',
@@ -79,7 +79,7 @@ const data: HabitYearData = {
         },
         {
           name: 'Write Down Journal Same Day',
-          values: '//////+',
+          values: '//////++',
         },
       ],
     },
--- src/pages/knowledge/growth/range/notes-1.tsx
+++ src/pages/knowledge/growth/range/notes-1.tsx
@@ -49,8 +49,61 @@ As above, scientific research suffers due to a lack of high-level perspective.
 Laszlo Polgar is the father of very successful chess prodigies, including the best female chess player of all time - Susan Polgar.  
 He implemented a specialist approach, with an early start.  
 He postulated that if more people used his approach to education, humanity would be better able to tackle great problems.  
+This books suggests that opposite of what Laszlo Polgar assumed might be the case for many domains.
 
+### Kind Learning Environments
 
+Some domains seem to be good for specialization.  
 
-This books suggests that opposite might be the case.
+This may include chess, firefighting, surgery, piloting. 
+ 
+The have the following characteristics.  
+They are narrow domains.  
+They have clearly defined rules, boundaries and goals.  
+Feedback is immediate, or close to that.  
+They are exhibiting repeating patterns.
+  
+Thus, they benefit greatly from experience.  
+However, these domains are the exception, not the rule.  
+The above are called 'kind' environments, as opposed to 'wicked'.
+
+### Wicked Learning Environments
+
+Rules are unclear or incomplete.  
+There may or may not be repetitive patterns, and they may not be obvious.  
+Feedback is often delayed, inaccurate, or both.  
+
+Sometimes, in wicked environments, experience will be a detriment, it might reinforce the exact wrong lessons.
+
+### AI
+
+AI is making specialized skills more obsolete.
+
+Human-computer combination performs better than either humans or computers alone.  
+It does so by making use of what each respective side does better.  
+Humans are (still) better at high level strategy, big picture, generalized thinking.  
+Computer are better at processing large amounts of data, memory, doing repetitive tasks, and lower level tactics.
+
+### Memorizing Patterns
+
+Memory capability is dependent on our ability to abstract away what we want to remember.
+  
+We remember familiar patterns.  
+It is much easier to remember a coherent sentence, than a jumble of random words, even if the total count of works is the same.  
+High level chess players have a much harder time remembering an impossible chess board positions than realistic ones.  
+This is especially pronounced with savants.
+
+### Lessons
+
+Trying to learn rules and patterns in wicked systems can easily backfire.  
+People may try to apply rules to a situation where it does not apply. They may also get defensive if it does not work.
+
+It is a bad idea to teach people to discover rules by providing rewards for repetitive short term success.  
+It will make them inflexible, as opposed to have them try to find a general solution.  
+Book contains a description of an experiment around page 28.
+
+In short, the stories of Polgars and Tiger Woods are wrong stories for what you will need to do in real life, which will require more flexibility.  
+In practice, it is much better to have more general, cross-domain competences.
+
+## Chapter 2 - How the Wicked World Was Made
 `;
