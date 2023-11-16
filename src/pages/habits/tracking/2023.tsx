2145f76cde4053ebae12236b1dc79ab0ee927029
Goran Mržljak <goran.mrzljak@gmail.com>
2023-11-16 08:52:07 +0100

    update

--- src/pages/habits/tracking/2023.tsx
+++ src/pages/habits/tracking/2023.tsx
@@ -621,51 +621,51 @@ const data: HabitYearData = {
       habits: [
         {
           name: 'Test Habit',
-          values: '------+---++--',
+          values: '------+---++---',
         },
         {
           name: 'Get out of Bed by 09:00',
-          values: '++-+-++++-+--+',
+          values: '++-+-++++-+--+-',
         },
         {
           name: 'No Sweets',
-          values: '++++++++++-+++',
+          values: '++++++++++-++++',
         },
         {
           name: 'No Unhealthy Food',
-          values: '+++++++++++-++',
+          values: '+++++++++++-+++',
         },
         {
           name: 'Go to Gym (M/W/F)',
-          values: '+.0==+.+.+..+.',
+          values: '+.0==+.+.+..+.+',
         },
         {
           name: 'Running 5km (T/T/S)',
-          values: '.+.+..+.+.+..+',
+          values: '.+.+..+.+.+..+.',
         },
         {
           name: 'Running - Before 08:45',
-          values: ',+.+..+.+.+..+',
+          values: ',+.+..+.+.+..+.',
         },
         {
           name: 'Stretch (T/T/S)',
-          values: '.+.+..+.+.+..+',
+          values: '.+.+..+.+.+..+.',
         },
         {
           name: 'Stretch - Before 09:30',
-          values: ',+.+..+.+.+..+',
+          values: ',+.+..+.+.+..+.',
         },
         {
           name: 'Neck Exercise',
-          values: ',,,,-,,,,,,-,,',
+          values: ',,,,-,,,,,,-,,,',
         },
         {
           name: 'Neck Exercise - Before 11:00',
-          values: ',,,,-,,,,,,-,,',
+          values: ',,,,-,,,,,,-,,,',
         },
         {
           name: 'Write Down Journal Before 12:00 Next Day',
-          values: '+--+-+++---+-+',
+          values: '+--+-+++---+-++',
         },
       ],
     },
